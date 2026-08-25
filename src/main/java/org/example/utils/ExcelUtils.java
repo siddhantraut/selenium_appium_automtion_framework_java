716a6d99b915fa03268c75fa70296fdd20510df9
hitalmarathe_04 <hitmarathe04@gmail.com>
2026-08-24 21:16:23 -0400

    Update ExcelUtils.java

--- src/main/java/org/example/utils/ExcelUtils.java
+++ src/main/java/org/example/utils/ExcelUtils.java
@@ -8,18 +8,12 @@ public class ExcelUtils {
     public static String getCellData(String filePath, String sheetName, int rowNumber, int columnNumber) throws IOException {
         FileInputStream file = new FileInputStream(filePath);
         Workbook workbook = WorkbookFactory.create(file);
-
         Sheet sheet = workbook.getSheet(sheetName);
-
         Row row = sheet.getRow(rowNumber);
-
         Cell cell = row.getCell(columnNumber);
-
         String data = cell.toString();
-
         workbook.close();
         file.close();
-
         return data;
     }
 }
