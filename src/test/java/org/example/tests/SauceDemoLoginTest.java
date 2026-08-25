2a09bd8bd47599c189cce0e1fa03d00940454c27
hitalmarathe_04 <hitmarathe04@gmail.com>
2026-08-24 21:04:59 -0400

    Update SauceDemoLoginTest.java

--- src/test/java/org/example/tests/SauceDemoLoginTest.java
+++ src/test/java/org/example/tests/SauceDemoLoginTest.java
@@ -1,12 +1,9 @@
 package org.example.tests;
-
 import org.example.base.BaseTest;
 import org.example.utils.ExcelUtils;
 import org.testng.annotations.Test;
 import pages.LoginPage;
-
 import java.io.IOException;
-
 
 public class SauceDemoLoginTest extends BaseTest {
     String filePath = "src/test/resources/testdata/SaucedemoLogin.xlsx";
@@ -17,19 +14,14 @@ public class SauceDemoLoginTest extends BaseTest {
             // Read username and password from Excel
             String username = ExcelUtils.getCellData(filePath,
                     "Login", 1, 0);
-
             String password = ExcelUtils.getCellData(filePath,
                     "Login", 1, 1);
-
             // Create LoginPage object
             LoginPage loginPage = new LoginPage(webDriver);
-
             // Login
             loginPage.enterUsername(username);
             loginPage.enterPassword(password);
             loginPage.clickLogin();
-
-
         } catch (Exception e) {
             System.err.println("Error in test: " + e.getMessage());
             throw e;
